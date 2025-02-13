4cdf12c59273cb71437c730bc42e08008a4f0a84
j$ <55032731+60fov@users.noreply.github.com>
2025-02-13 00:19:13 -0600

    studylove

--- src/pages/StudyLove.tsx
+++ src/pages/StudyLove.tsx
@@ -1,4 +1,4 @@
-import { SetStoreFunction, createStore, produce } from 'solid-js/store';
+import { SetStoreFunction, createStore, produce, unwrap } from 'solid-js/store';
 import * as StudyLoveApp from './studylove/app';
 
 import styles from './StudyLove.module.css';
@@ -56,13 +56,23 @@ export default function StudyLove() {
     const handleGameChoiceClick: JSX.EventHandler<HTMLLIElement, MouseEvent> = (event) => {
         const selectedGame = event.currentTarget.getAttribute("data-game");
         if (selectedGame) {
-            {
-                const tr = StudyLoveApp.Transaction.create("state", "set", "game_hobby");
+            if (!state.playedGameList.includes(selectedGame as StudyLoveApp.GameName)) {
+                const tr = StudyLoveApp.Transaction.create("state", "set", `game_${selectedGame}`);
                 runTransaction(tr);
+            } else {
+                console.error(`choose game: tried to play game that has been played (${selectedGame})`);
             }
             // StudyLoveApp.selectGame(testAppState, selectedGame);
         } else {
             console.error("clicked on game choice but there is no data-game value on that element");
+        }
+    };
+
+    const handlePhoneButtonClick: JSX.EventHandler<HTMLDivElement, MouseEvent> = (event) => {
+        console.log("ui: phone button click");
+        {
+            const tr = StudyLoveApp.Transaction.create("state", "set", "phone_number");
+            runTransaction(tr);
         }
     };
 
@@ -72,7 +82,7 @@ export default function StudyLove() {
             const tr = StudyLoveApp.Transaction.create("state", "set", "intro");
             runTransaction(tr);
         }
-    }
+    };
 
     const handleIntro: JSX.EventHandler<HTMLButtonElement, MouseEvent> = (event) => {
         console.log("ui: handle intro");
@@ -98,13 +108,13 @@ export default function StudyLove() {
                 <button onClick={() => prevTransaction()}>prev state</button>
                 <button onClick={() => nextTransaction()}>next state</button>
             </div> */}
-            <div class={styles.game} style={{"background": `url(${state.background})`}}>
+            <div class={styles.game} style={{ "background": `url(${state.background})` }}>
                 <div class={styles.npc}>
                     <img src={state.npc.asset} />
-                    </div>
+                </div>
                 {state.state === "start" ? (
                     <div class={styles.startScreen} onClick={handleStart}>
-                        click to start
+                        <img src="src/assets/imgs/title.webp" width="100%" />
                     </div>
                 ) : state.state === "intro" ? (
                     <>
@@ -113,10 +123,16 @@ export default function StudyLove() {
                             <button class={styles.choiceButton} onClick={handleFlee}>flee</button>
                         </div>
                     </>
-                ) : state.state === "end" ? (
+                ) : state.state === "end_win" ? (
                     <>
                         <div class={styles.endScreen} onClick={handleEndScreenClick}>
-                            game over click to play again
+                            nice job getting their number, click to play again
+                        </div>
+                    </>
+                ) : state.state === "end_lose" ? (
+                    <>
+                        <div class={styles.endScreen} onClick={handleEndScreenClick}>
+                            you failed to get their number, click to play again
                         </div>
                     </>
                 ) : (
@@ -125,7 +141,7 @@ export default function StudyLove() {
                             {/* TODO: turn into progress bar */}
                             <div class={styles.meter} style={{ "--meter-value": `${state.meter.value}%` }}></div>
                             <div class={styles.textBox}>{StudyLoveApp.getSpeechBubbleText(state)}</div>
-                            <div class={styles.phoneButton}></div>
+                            <div class={styles.phoneButton} onClick={handlePhoneButtonClick}></div>
                         </div>
                         <div class={styles.lower}>
                             {/* TODO: render games here */}
@@ -137,7 +153,11 @@ export default function StudyLove() {
                                 </ul>
                             ) : state.state === "game_hobby" ? (
                                 <>{renderHobby(state, setState)}</>
-                            ) : <></> }
+                            ) : state.state === "game_mbti" ? (
+                                <>{renderMBTI(state, setState)}</>
+                            ) : state.state === "phone_number" ? (
+                                <>{renderPhone(state, setState)}</>
+                            ) : <></>}
                         </div>
                     </>
                 )}
@@ -148,38 +168,41 @@ export default function StudyLove() {
 
 
 function renderHobby(state: StudyLoveApp.ApplicationModel, setState: SetStoreFunction<StudyLoveApp.ApplicationModel>) {
-
-        createEffect(() => {
-            const list = state.hobbyGame.selectedWordList;
-            const list1 = state.npc.hobbies;
-           if (list1.length === list.length) {
+    createEffect(() => {
+        const list = state.hobbyGame.selectedWordList;
+        const list1 = state.npc.hobbies;
+        if (list1.length === list.length) {
+            setState(produce((state) => {
+                state.ledger.push(StudyLoveApp.Transaction.create("game_played", "", "hobby"));
+                StudyLoveApp.Transaction.run(state, state.ledger.slice(state.ledgerNdx));
+            }));
             setState(produce((state) => {
                 state.ledger.push(StudyLoveApp.Transaction.create("state", "set", "choose_game"));
                 StudyLoveApp.Transaction.run(state, state.ledger.slice(state.ledgerNdx));
             }));
-           }
-        });
+        }
+    });
 
-        const handleEmojiClick: JSX.EventHandler<HTMLDivElement, MouseEvent> = (event) => {
-            const wordId = event.currentTarget.getAttribute("data-word-id");
-            if (wordId) {
-                const word = WordList.getWordById(wordId);
-                if (word !== undefined) {
-                    if (word.id === state.npc.hobbies[state.hobbyGame.selectedWordList.length].id) {
-                        setState(produce((state) => {
-                            state.ledger.push(StudyLoveApp.Transaction.create("meter", "set", state.meter.value + 15));
-                            StudyLoveApp.Transaction.run(state, state.ledger.slice(state.ledgerNdx));
-                        }));
-                    }
-                    setState("hobbyGame", produce((game) => {
-                        game.selectedWordList.push(word);
+    const handleEmojiClick: JSX.EventHandler<HTMLDivElement, MouseEvent> = (event) => {
+        const wordId = event.currentTarget.getAttribute("data-word-id");
+        if (wordId) {
+            const word = WordList.getWordById(wordId);
+            if (word !== undefined) {
+                if (word.id === state.npc.hobbies[state.hobbyGame.selectedWordList.length].id) {
+                    setState(produce((state) => {
+                        state.ledger.push(StudyLoveApp.Transaction.create("meter", "set", state.meter.value + 15));
+                        StudyLoveApp.Transaction.run(state, state.ledger.slice(state.ledgerNdx));
                     }));
-                } else {
-                    console.error("hobby game: emoji clicked and data-word-id doesnt corresponsed to a word in the word list");
                 }
+                setState("hobbyGame", produce((game) => {
+                    game.selectedWordList.push(word);
+                }));
             } else {
-                console.error("hobby game: emoji clicked w/o data-word-id attribute???");
+                console.error("hobby game: emoji clicked and data-word-id doesnt corresponsed to a word in the word list");
             }
+        } else {
+            console.error("hobby game: emoji clicked w/o data-word-id attribute???");
+        }
     };
 
     return (
@@ -189,6 +212,138 @@ function renderHobby(state: StudyLoveApp.ApplicationModel, setState: SetStoreFun
             }</For>
         </div>
     )
+}
+
+function renderPhone(state: StudyLoveApp.ApplicationModel, setState: SetStoreFunction<StudyLoveApp.ApplicationModel>) {
+    const [phoneNumber, setPhoneNumber] = createSignal<string>("");
+
+    const handleNumClick: JSX.EventHandler<HTMLButtonElement, MouseEvent> = (event) => {
+        const num = event.currentTarget.getAttribute("data-number");
+        if (num) {
+            if (phoneNumber().length < 7) {
+                setPhoneNumber((prev) => prev + num);
+            }
+        } else {
+            console.error("phone: number clicked but no data-number attribute???");
+        }
+    };
+
+    const handleDel: JSX.EventHandler<HTMLButtonElement, MouseEvent> = (event) => {
+        if (phoneNumber().length > 0) {
+            setPhoneNumber((prev) => prev.slice(0, prev.length - 1));
+        }
+    };
+
+    const handleSave: JSX.EventHandler<HTMLButtonElement, MouseEvent> = (event) => {
+        setState(produce((state) => {
+            state.ledger.push(StudyLoveApp.Transaction.create("number", "set", phoneNumber()));
+            StudyLoveApp.Transaction.run(state, state.ledger.slice(state.ledgerNdx));
+        }));
+    };
+
+    return (
+        <div class={styles.phone}>
+            <div data-num-entry>{phoneNumber()}</div>
+            <div data-numpad>
+                <button data-number="1" onClick={handleNumClick}>1</button>
+                <button data-number="2" onClick={handleNumClick}>2</button>
+                <button data-number="3" onClick={handleNumClick}>3</button>
+                <button data-number="4" onClick={handleNumClick}>4</button>
+                <button data-number="5" onClick={handleNumClick}>5</button>
+                <button data-number="6" onClick={handleNumClick}>6</button>
+                <button data-number="7" onClick={handleNumClick}>7</button>
+                <button data-number="8" onClick={handleNumClick}>8</button>
+                <button data-number="9" onClick={handleNumClick}>9</button>
+                <span></span>
+                <button data-number="0" onClick={handleNumClick}>0</button>
+                <button data-del onClick={handleDel}>del</button>
+            </div>
+            <button data-save onClick={handleSave}>save</button>
+        </div>
+    );
+}
+
+
+function renderMBTI(state: StudyLoveApp.ApplicationModel, setState: SetStoreFunction<StudyLoveApp.ApplicationModel>) {
+    const [mbti, setMbti] = createSignal<number[]>([0, 0, 0, 0]);
+    const [guessList, setGuessList] = createSignal<string[]>([]);
+
+    const mbtiTable = StudyLoveApp.mbtiTable;
+
+    function getMbtiString(): string {
+        return mbtiTable.reduce((prev, curr, ndx) => {
+            return prev + mbtiTable[ndx][mbti()[ndx]];
+        }, "");
+    }
+
+    createEffect(() => {
+        const outOfGuesses = guessList().length === 3;
+        if (outOfGuesses) {
+            // end game
+            setState(produce((state) => {
+                state.ledger.push(StudyLoveApp.Transaction.create("game_played", "", "mbti"));
+                StudyLoveApp.Transaction.run(state, state.ledger.slice(state.ledgerNdx));
+            }));
+            setState(produce((state) => {
+                state.ledger.push(StudyLoveApp.Transaction.create("state", "set", "choose_game"));
+                StudyLoveApp.Transaction.run(state, state.ledger.slice(state.ledgerNdx));
+            }));
+        }
+    });
+
+    const handleToggle: JSX.EventHandler<HTMLInputElement, Event> = (event) => {
+        const mbtiNdxAttrib = event.currentTarget.getAttribute("data-index");
+        if (mbtiNdxAttrib !== null) {
+            setMbti((prev) => {
+                const mbtiNdx = parseInt(mbtiNdxAttrib);
+                const result = [...prev];
+                result[mbtiNdx] = prev[mbtiNdx] === 0 ? 1 : 0;
+                return result;
+            });
+        } else {
+            console.error("mbti: tile clicked but no data-index attribute???");
+        }
+    };
+
+    const handleSubmit: JSX.EventHandler<HTMLButtonElement, Event> = (event) => {
+        const guessCorrect = getMbtiString() === state.npc.mbti;
+        console.log("getmbti()", getMbtiString(), "npc mbti", state.npc.mbti);
+        if (guessCorrect) {
+            // give points
+            setState(produce((state) => {
+                state.ledger.push(StudyLoveApp.Transaction.create("meter", "set", state.meter.value + 30));
+                StudyLoveApp.Transaction.run(state, state.ledger.slice(state.ledgerNdx));
+            }));
+            setState(produce((state) => {
+                state.ledger.push(StudyLoveApp.Transaction.create("state", "set", "choose_game"));
+                StudyLoveApp.Transaction.run(state, state.ledger.slice(state.ledgerNdx));
+            }));
+        }
+
+        setGuessList((prev) => [...prev, getMbtiString()]);
+    };
+
+    return (
+        <div class={styles.mbti}>
+            <div data-tile>
+                <label for="extra_intro">{mbtiTable[0][mbti()[0]]}</label>
+                <input id="extra_intro" type="checkbox" data-index={0} onChange={handleToggle} />
+            </div>
+            <div data-tile>
+                <label for="intu_obser">{mbtiTable[1][mbti()[1]]}</label>
+                <input id="intu_obser" type="checkbox" data-index={1} onChange={handleToggle} />
+            </div>
+            <div data-tile>
+                <label for="think_feel">{mbtiTable[2][mbti()[2]]}</label>
+                <input id="think_feel" type="checkbox" data-index={2} onChange={handleToggle} />
+            </div>
+            <div data-tile>
+                <label for="judge_prosp">{mbtiTable[3][mbti()[3]]}</label>
+                <input id="judge_prosp" type="checkbox" data-index={3} onChange={handleToggle} />
+            </div>
+            <button onClick={handleSubmit}>guess {guessList().length + 1} / 3</button>
+        </div>
+    );
 }
 
 /** dont use this */
--- src/pages/studylove/app.ts
+++ src/pages/studylove/app.ts
@@ -5,8 +5,10 @@ export const stateList = [
   "intro",
   "choose_game",
   "game_hobby",
+  "game_mbti",
   "phone_number",
-  "end",
+  "end_win",
+  "end_lose",
 ] as const;
 
 export type State = (typeof stateList)[number];
@@ -20,11 +22,11 @@ export type Meter = {
 };
 
 export type NPC = {
+  asset: string;
   name: string;
   phoneNumber: string;
   hobbies: WordList.Word[];
-  /** figure out what this is */
-  asset: string;
+  mbti: string;
   //   outfit?: string; // TBD
   //   mbti?: string; // TBD
 };
@@ -51,6 +53,7 @@ export const gameList = [
 ] as const;
 
 // TODO: how can i limit type to be one of game list
+// TODO: do we need game state on app state?
 export type HobbyGame = {
   type: "hobby";
   wordPool: WordList.Word[];
@@ -79,7 +82,9 @@ export type ApplicationModel = {
   time: number;
   meter: Meter;
   npc: NPC;
+  // TODO: remove this
   hobbyGame: HobbyGame;
+  playedGameList: GameName[];
 };
 
 export type PhraseTable = typeof phraseTable;
@@ -105,8 +110,10 @@ export const initalAppState: ApplicationModel = {
     asset: "",
     hobbies: [],
     phoneNumber: "",
+    mbti: "",
   },
-  hobbyGame: initialHobbyGameState,
+  hobbyGame: structuredClone(initialHobbyGameState),
+  playedGameList: [],
 };
 
 export type StateTransaction = {
@@ -114,15 +121,6 @@ export type StateTransaction = {
   name: string;
   data?: any;
 };
-
-export function getSpeechBubbleText(app: ApplicationModel): string {
-  switch(app.state) {
-    case "game_hobby": {
-      return `${app.npc.hobbies[0].korean}, ${app.npc.hobbies[1].korean}, 그리고 ${app.npc.hobbies[2].korean} 좋아합니다`
-    }
-  }
-  return '';
-}
 
 export const Transaction = {
   create(type: string, name: string, data?: any): StateTransaction {
@@ -135,6 +133,22 @@ export const Transaction = {
     for (const tr of transactionList) {
       console.log("running tr", tr);
       switch (tr.type) {
+        case "game_played": {
+          if (gameList.map((game) => game.name).includes(tr.data as GameName)) {
+            app.playedGameList.push(tr.data);
+          }
+          break;
+        }
+        case "number": {
+          if (tr.name === "set") {
+            if (app.npc.phoneNumber === tr.data) {
+              app.state = "end_win";
+            } else {
+              app.state = "end_lose";
+            }
+          }
+          break;
+        }
         case "npc": {
           if (tr.name === "set") {
             app.npc = tr.data;
@@ -146,13 +160,23 @@ export const Transaction = {
             // TODO: data validation
             app.state = tr.data;
             if (tr.data === "intro") {
+              // app = structuredClone(initalAppState);
+              app.hobbyGame = structuredClone(initialHobbyGameState);
+              app.meter.value = 0;
               app.npc = generateNpc();
+              console.log(app.npc);
               app.background = getRandomBackground();
-              console.log(app);
             } else if (tr.data === "game_hobby") {
               const hobbyList = WordList.getWordsByCategory("hobbies");
-              const filteredWordList = hobbyList.filter((wordListWord) => !app.npc.hobbies.find(npcHobby => npcHobby.id === wordListWord.id));
-              app.hobbyGame.wordPool = shuffle(app.npc.hobbies.concat(filteredWordList).slice(0, 6));
+              const filteredWordList = hobbyList.filter(
+                (wordListWord) =>
+                  !app.npc.hobbies.find(
+                    (npcHobby) => npcHobby.id === wordListWord.id
+                  )
+              );
+              app.hobbyGame.wordPool = shuffle(
+                app.npc.hobbies.concat(filteredWordList).slice(0, 6)
+              );
               // console.log(app);
             }
           }
@@ -175,8 +199,10 @@ export const Transaction = {
 };
 
 function getRandomBackground(): string {
-  const fn = ["alley", "sunset_street", "school_hallway"][Math.floor(Math.random() * 3)];
-  return `src/assets/imgs/bg_${fn}.png`
+  const fn = ["alley", "sunset_street", "school_hallway"][
+    Math.floor(Math.random() * 3)
+  ];
+  return `src/assets/imgs/bg_${fn}.png`;
 }
 
 function generateNpc(): NPC {
@@ -184,10 +210,133 @@ function generateNpc(): NPC {
     asset: `src/assets/imgs/char${Math.ceil(Math.random() * 3)}.png`,
     hobbies: shuffle(WordList.getWordsByCategory("hobbies")).slice(0, 3),
     name: shuffle(["mina", "kate", "ashley"]).at(0)!,
-    phoneNumber: Array.from({length: 7}).map(() => Math.floor(Math.random() * 10)).join(""),
-  }
+    phoneNumber: Array.from({ length: 7 })
+      .map(() => Math.floor(Math.random() * 10))
+      .join(""),
+    mbti: getRandomMbti(),
+  };
 }
 
-function shuffle<T>(arr: T[]) : T[] {
+function getRandomMbti(): string {
+  return mbtiTable.reduce((prev, curr, ndx) => {
+    return prev + mbtiTable[ndx][Math.floor(Math.random() * 2)];
+  }, "");
+}
+
+function shuffle<T>(arr: T[]): T[] {
   return [...arr].sort(() => 0.5 - Math.random());
 }
+
+/** TODO: speech bubble text should instead be set by transactions */
+export function getSpeechBubbleText(app: ApplicationModel): string {
+  switch (app.state) {
+    case "intro": {
+      return `안녕하세요, 저는 ${app.npc.name}입니다. 만나서 반가워요!`;
+    }
+    case "game_hobby": {
+      return `${app.npc.hobbies[0].korean}, ${app.npc.hobbies[1].korean}, 그리고 ${app.npc.hobbies[2].korean} 좋아합니다`;
+    }
+    case "phone_number": {
+      return `${app.npc.phoneNumber
+        .split("")
+        .map((num) => numberList[parseInt(num)])
+        .join("")}`;
+    }
+    case "game_mbti": {
+      return `${mbtiPhraseTable[app.npc.mbti][0]}`;
+    }
+  }
+  return "";
+}
+
+const numberList = ["공", "일", "이", "삼", "사", "오", "육", "칠", "발", "구"];
+
+export const mbtiTable = [
+  ["E", "I"],
+  ["N", "S"],
+  ["T", "F"],
+  ["J", "P"],
+];
+
+const mbtiPhraseTable: Record<string, string[]> = {
+  ISTJ: [
+    "나는 혼자 있고 계획하는 걸 좋아해요.",
+    "감정보다 사실이 중요해요.",
+    "나는 변화를 싫어하고 규칙을 잘 지켜요.",
+  ],
+  ISFJ: [
+    "나는 조용한 곳이 좋고 남을 돕고 싶어요.",
+    "나는 변화를 싫어하고 계획이 필요해요.",
+    "나는 세심하고 책임감이 많아요.",
+  ],
+  INFJ: [
+    "나는 혼자 생각하고 깊은 이야기를 좋아해요.",
+    "나는 감정을 잘 이해하고 미래를 꿈꿔요.",
+    "나는 의미를 찾고 가능성을 생각해요.",
+  ],
+  INTJ: [
+    "나는 혼자 공부하고 미래를 계획해요.",
+    "나는 감정보다 논리가 중요해요.",
+    "나는 목표가 필요하고 효율이 좋아요.",
+  ],
+  ISTP: [
+    "나는 혼자가 편하고 직접 해보는 게 좋아요.",
+    "나는 감정보다 효율이 중요하고 즉흥적이에요.",
+    "나는 이론보다 실험을 좋아하고 말이 적어요.",
+  ],
+  ISFP: [
+    "나는 혼자 있는 게 좋고 감성이 많아요.",
+    "나는 자유롭게 살고 순간을 즐겨요.",
+    "나는 내 방식이 좋고 남을 돕는 게 좋아요.",
+  ],
+  INFP: [
+    "나는 상상하고 감정을 중요하게 생각해요.",
+    "나는 현실보다 가능성을 보고 자유롭게 살아요.",
+    "나는 꿈이 크고 나만의 세계가 있어요.",
+  ],
+  INTP: [
+    "나는 혼자 생각하고 아이디어를 분석해요.",
+    "나는 감정보다 논리가 중요하고 자유롭게 생각해요.",
+    "나는 새로운 방법을 찾고 깊이 고민해요.",
+  ],
+  ESTP: [
+    "나는 가만히 있는 게 싫고 직접 해봐야 해요.",
+    "나는 감정보다 논리가 중요하고 즉흥적이에요.",
+    "나는 행동이 중요하고 현실적으로 생각해요.",
+  ],
+  ESFP: [
+    "나는 사람들과 어울리고 분위기를 즐겨요.",
+    "나는 즉흥적이고 감정을 솔직하게 표현해요.",
+    "나는 계획보다 지금을 즐기고 에너지가 많아요.",
+  ],
+  ENFP: [
+    "나는 사람들과 이야기하고 새로운 걸 배워요.",
+    "나는 감정을 중요하게 생각하고 자유롭게 살아요.",
+    "나는 아이디어가 많고 모험을 좋아해요.",
+  ],
+  ENTP: [
+    "나는 토론하는 게 좋고 아이디어가 많아요.",
+    "나는 감정보다 논리가 중요하고 정해진 방법을 싫어해요.",
+    "나는 도전을 좋아하고 변화를 즐겨요.",
+  ],
+  ESTJ: [
+    "나는 사람들과 함께 일하고 현실적인 게 좋아요.",
+    "나는 감정보다 논리가 중요하고 계획이 필요해요.",
+    "나는 규칙을 지키고 목표를 이루고 싶어요.",
+  ],
+  ESFJ: [
+    "나는 사람들과 함께 있고 감정을 잘 표현해요.",
+    "나는 분위기를 맞추고 계획대로 움직여요.",
+    "나는 단체 활동을 좋아하고 남을 돕는 게 좋아요.",
+  ],
+  ENFJ: [
+    "나는 사람들과 이야기하고 감정을 잘 이해해요.",
+    "나는 미래를 생각하고 계획을 세워요.",
+    "나는 분위기를 조절하고 사람들을 도와요.",
+  ],
+  ENTJ: [
+    "나는 사람들과 일하고 목표를 중요하게 생각해요.",
+    "나는 논리적으로 생각하고 미래를 계획해요.",
+    "나는 효율을 중요하게 생각하고 리더십이 있어요.",
+  ],
+};
